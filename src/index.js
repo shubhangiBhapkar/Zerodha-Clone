e15bf2fd2e6c694e0f5c11ff9d30dc46979f5a0d
web Development <shubhangibhapkar35@gmail.com>
2025-09-13 00:35:57 +0530

    About Page

--- src/index.js
+++ src/index.js
@@ -2,37 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/Home2/HomePage';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-// import signup from "./landing_page/signup/signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductsPage from './landing_page/products/ProductsPage';
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
-
 import PageNotFound from "./landing_page/PageNotFound";
 import Navbar from "./landing_page/Navbar";
 import Footer from './landing_page/Footer';
 
-import {Link} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<HomePage/>}></Route>
-          {/* <Route path="/signup" element={<signup/>}></Route> */}
-          <Route path="/about" element={<AboutPage/>}></Route>
-          <Route path="/products" element={<ProductsPage/>}></Route>
-          <Route path="/pricing" element={<PricingPage/>}></Route>
-          <Route path="/support" element={<SupportPage/>}></Route>
-          <Route path="*" element={<PageNotFound/>}></Route>
-        </Routes>
-      <Footer/>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<PageNotFound/>} />
+    </Routes>
+    <Footer />
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
